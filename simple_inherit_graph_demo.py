23b24a3e0c801a6acf6982c727256e384a9e0076
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2013-04-07 01:40:27 -0700

    Revert the edge direction.

--- simple_inherit_graph_demo.py
+++ simple_inherit_graph_demo.py
@@ -15,7 +15,7 @@ def build():
     tu = index.parse(cpp_file_path)
     G = pygraphviz.AGraph(directed=True)
     for this, parent in ir.each_inheritance_relation(tu.cursor):
-        edge = (parent, this)
+        edge = (this, parent)
         G.add_edge(edge)
     return G
 
